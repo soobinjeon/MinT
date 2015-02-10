f821008382aab926391f7e97bfdd96f75765cc01
soobin Jeon <j.soobin@gmail.com>
2015-02-10 10:57:40 +0900

    init Project

---
+++ framework/main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: soobin Jeon, Chungsan Lee, YoungTak Han
+ *
+ * Created on February 10, 2015, 10:01 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Hello World\n");
+    printf("Hello World\n");
+    return 0;
+}
+
