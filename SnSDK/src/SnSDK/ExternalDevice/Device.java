96511e1a0d10634c9087c6dc6960c3dca4c37d04
HanYoungTak <gksdudxkr@gmail.com>
2015-05-08 15:11:47 +0900

    Updated - Framework Source Added PortPinSet.java Updated Device.java -Added ArrayList<PortPinSet> ppSet -Added Method registPortPin, registADCPortPin, registUARTPortPin, getPortPinList

--- SnSDK/src/SnSDK/ExternalDevice/Device.java
+++ SnSDK/src/SnSDK/ExternalDevice/Device.java
@@ -1,7 +1,6 @@
 /*
  * Copyright (C) 2015 Software&System Lab. Kangwon National University.
  *
-
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -16,6 +15,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package SnSDK.ExternalDevice;
+
+import java.util.ArrayList;
+
 /**
  *
  * @author soobin Jeon <j.soobin@gmail.com>, chungsan Lee <dj.zlee@gmail.com>,
@@ -23,21 +25,17 @@ package SnSDK.ExternalDevice;
  */
 public class Device {
 
-    private int[] port;
+    protected ArrayList<PortPinSet> ppSet = new ArrayList<>();
     private String Library_Name = null;
 
-    public Device(String _LibName, int[] port) {
+    public Device(String _LibName) {
         super();
         Library_Name = _LibName;
-        initPort(port);
-    }
-
-    private void initPort(int[] _port) {
-        port = _port;
     }
 
     /**
-     * 센서 초기화
+     * Initiallize Sensor
+     * - It loads library
      */
     public void initDevice() {
         LoadLibrary();
@@ -61,13 +59,77 @@ public class Device {
     protected String getLibraryName() {
         return this.Library_Name;
     }
-
-    protected boolean hasPort() {
-        if (port != null && port.length > 0) {
-            return true;
-        } else {
-            return false;
+    
+    /**
+     * Regist using Port, Pin number
+     * - add port, pin set
+    */
+    public void registPortPin(int port, int pin)
+    {
+        ppSet.add(new PortPinSet(port, pin));
+    }
+    
+    public void registUARTPortPin(int uartNumber)
+    {
+        PortPinSet uartTx = null;
+        PortPinSet uartRx = null;
+        switch(uartNumber)
+        {
+            case 1:
+                uartTx = new PortPinSet(9, 24);
+                uartRx = new PortPinSet(9, 26);
+                break;
+            case 2:
+                uartTx = new PortPinSet(9, 21);
+                uartRx = new PortPinSet(9, 22);
+                break;
+            case 4:
+                uartTx = new PortPinSet(9, 13);
+                uartRx = new PortPinSet(9, 11);
+                break;
+            case 5:
+                uartTx = new PortPinSet(8, 37);
+                uartRx = new PortPinSet(8, 38);
+                break;
         }
+        ppSet.add(uartTx);
+        ppSet.add(uartRx);
+    }
+    
+    public void registADCPortPin(int adcNumber)
+    {
+        PortPinSet adcPPS = null;
+        switch(adcNumber)
+        {
+            case 0:
+                adcPPS = new PortPinSet(9, 39);
+                break;
+            case 1:
+                adcPPS = new PortPinSet(9, 40);
+                break;
+            case 2:
+                adcPPS = new PortPinSet(9, 37);
+                break;
+            case 3:
+                adcPPS = new PortPinSet(9, 38);
+                break;
+            case 4:
+                adcPPS = new PortPinSet(9, 33);
+                break;
+            case 5:
+                adcPPS = new PortPinSet(9, 36);
+                break;
+            case 6:
+                adcPPS = new PortPinSet(9, 35);
+                break;
+        }
+    
+        ppSet.add(adcPPS);
+    }
+
+    public ArrayList<PortPinSet> getPortPinList()
+    {
+        return ppSet;
     }
 
 }
